0efefc9b51496173a6f4b976ec513dbf69befa39
JoelBoban <90124641+JoelBoban@users.noreply.github.com>
2021-10-17 15:07:33 +0530

    Update Simple_Config.py

--- DonLee_Robot/Simple_Config.py
+++ DonLee_Robot/Simple_Config.py
@@ -4,7 +4,7 @@ class Mo_Tech_YT(object):
     MO_TECH_YT_01 = InlineKeyboardMarkup
     MO_TECH_YT_02 = InlineKeyboardButton
     MO_TECH_YT_03 = os.environ.get("DB_URI")
-    MO_TECH_YT_04 = os.environ.get("DB_NAME", "mOtech")
+    MO_TECH_YT_04 = os.environ.get("DB_NAME", "suraj")
     MO_TECH_YT_05 = os.environ.get("EXPELL").split()
     MO_TECH_YT_06 = os.environ.get("KITTUMO")
     MO_TECH_YT_07 = bool(os.environ.get("SIZE_BUTTON"))
@@ -13,7 +13,7 @@ class Mo_Tech_YT(object):
     MO_TECH_YT_10 = int(os.environ.get("APP_ID"))
     MO_TECH_YT_11 = os.environ.get("API_HASH")
     MO_TECH_YT_12 = os.environ.get("BOT_TOKEN")
-    MO_TECH_YT_13 = os.environ.get("PHOTTO", "https://telegra.ph/file/7d04d3370126136c9c7a9.jpg")
-    MO_TECH_YT_14 = os.environ.get("FSUB_TEXT", "Join My Update Channel")
-    MO_TECH_YT_15 = os.environ.get("FORCES_SUB", "Mo_Tech_YT")
+    MO_TECH_YT_13 = os.environ.get("PHOTTO", "https://telegra.ph/SERIES-MOVIES-10-17")
+    MO_TECH_YT_14 = os.environ.get("FSUB_TEXT", "Join My Group")
+    MO_TECH_YT_15 = os.environ.get("FORCES_SUB", "@seriesmovieszz")
     MO_TECH_YT_16 = os.environ.get("USER_SESSION")
