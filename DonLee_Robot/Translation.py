cbfed2b225d5f66db31b1d56c3e70179ebf7e012
sooraj20055 <92521957+sooraj20055@users.noreply.github.com>
2021-10-17 12:04:18 +0530

    Update Translation.py

--- DonLee_Robot/Translation.py
+++ DonLee_Robot/Translation.py
@@ -5,10 +5,10 @@
 class Translation(object):
     
     START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
-<b>Am Just A Donlee Adv Auto Filter Bot....😉</b>
+<b>Am a All In One Filter Bot....🎈</b>
 
-<b>Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝</b>
-<b>Watch the video below to build an DonLee Adv Autofilter bot like me...</b>
+<b>Just Add Me To Your Group And Channel And Connect Them And See My power ⚡</b>
+<b>Watch the video below to build an All In One filter bot like me...</b>
 <b>Subscribe to the update channel to learn about my updates and activity...</b>
 <b>Press /help to know about available commands🤪</b>"""    
    
@@ -42,16 +42,16 @@ class Translation(object):
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@Mo_Tech_YT
+@seriesmovieszz
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/Mrk_YT">Muhammed Rk</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/allinone_file/14">Sooraj Be</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/PR0FESS0R-99/DonLee_Robot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/sooraj20055/allinonev1">Click Me</a></i>
 """
