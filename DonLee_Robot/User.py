98d73aa5e333a73d417638767ec5268209986d6b
sooraj20055 <92521957+sooraj20055@users.noreply.github.com>
2021-10-17 12:05:24 +0530

    Update User.py

--- DonLee_Robot/User.py
+++ DonLee_Robot/User.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG
-# (e) @Muhammed_RK, @Mo_Tech_YT , @Mo_Tech_Group, @MT_Botz
+# (e) @@sooraj_be, @seriesmovieszz , @@allinonemsbot, @seriesmovieszz
 # Copyright permission under MIT License
 # All rights reserved by PR0FESS0R-99
 # License -> https://github.com/PR0FESS0R-99/DonLee_Robot/blob/main/LICENSE
