7bbb32bdea46f4078bf7a0643ccd1dfdb86c13d6
sooraj20055 <92521957+sooraj20055@users.noreply.github.com>
2021-10-17 13:46:22 +0530

    Update Settings.py

--- DonLee_Robot/Plugins/Settings.py
+++ DonLee_Robot/Plugins/Settings.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) @AlbertEinsteinTG
-# (e) @@sooraj_be, @@allinonemsbot , @seriesmovieszz, @@allinonemsbot
+# (e) @sooraj_be, @allinonemsbot , @seriesmovieszz, @allinonemsbot
 # Copyright permission under MIT License
 # All rights reserved by PR0FESS0R-99
 # License -> https://github.com/PR0FESS0R-99/DonLee_Robot/blob/main/LICENSE
